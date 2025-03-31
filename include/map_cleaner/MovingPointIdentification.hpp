4a90baa8d1c4b97aa60acb227837e95364c11b74
kamibukuro5656 <amacoa913@gmail.com>
2025-03-31 16:39:01 +0900

    fix

--- include/map_cleaner/MovingPointIdentification.hpp
+++ include/map_cleaner/MovingPointIdentification.hpp
@@ -141,10 +141,10 @@ private:
 
     if(counter[0] != 0)
       return FusedResult::CASE_A;
-    else if(counter[2] != 0)
-      return FusedResult::CASE_C;
     else if(counter[1] != 0)
       return FusedResult::OTHERWISE;
+    else if(counter[2] != 0)
+      return FusedResult::CASE_C;
     
     return FusedResult::OTHERWISE;
   }
